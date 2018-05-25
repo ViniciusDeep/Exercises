c67cadbd7caacfd2557653859877f1bfd0bff238
Vinicius Deep <viniciuscorreia11@gmail.com>
2018-05-25 20:21:46 -0300

    Implemented function convertTo Xor

--- C/1- Operadores Lógicos.cpp
+++ C/1- Operadores Lógicos.cpp
@@ -2,6 +2,7 @@
  
 int opOr(int a, int b); 
 int opAnd(int a, int b);
+int opXor(int a, int b);
 
  
 int main() {
@@ -11,6 +12,7 @@ int main() {
 	scanf("%d%d",&a,&b);
  	 printf("%d", opAnd(a,b));
 	 printf("%d", opOr(a,b));
+	 printf("%d", opXor(a,b));
  	
  	
  	
@@ -40,32 +42,40 @@ int opAnd(int a, int b) {
 
 
 int opOr(int a, int b) {
-	int r = a | b,q, i = 0, vet[32], j;
+	int r = a | b, i = 0, j, q, vet[32];
 	
-		do {
-			q = r / 2;
-			vet[i] = r % 2;
-			r = q;
-			i++;
-			} while (q != 1 && q > 0);
-				vet[i] = r; 
-				printf("\nOR: ");
-			for (j = i; j >= 0; j--) { 
-				printf("%d", vet[j]);
-	}
+	do {
+		q = r / 2;
+		vet[i] = r % 2;
+		r = q;
+		i++;
+	} while(q != 1 && q > 0);
+		vet[i] = r;
+		printf("\nOR: ");
+	for(j = i; j > 0; j--) {
+		printf("%d", vet[j]);
+	}	
 	
 	return 0;
 }
 
 
-int OpXor(int a, int b) {
-	int r = a ^ b, q, vet[32], j;
+int opXor(int a, int b) {
+		int r = a ^ b, i = 0, j, q, vet[32];
 	
 	do {
-		
-	}
+		q = r / 2;
+		vet[i] = r % 2;
+		r = q;
+		i++;
+	} while(q != 1 && q > 0);
+		vet[i] = r;
+		printf("\nXOR: ");
+	for(j = i; j >= 0; j--) {
+		printf("%d", vet[j]);
+	}	
 	
-	
+	return 0;
 }
 
 
