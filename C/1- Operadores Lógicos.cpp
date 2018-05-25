1f8e683a0e459cc2ca4c01d533eac8597c36caa9
Vinicius Deep <viniciuscorreia11@gmail.com>
2018-05-25 20:03:41 -0300

    Create other function to convert

--- C/1- Operadores Lógicos.cpp
+++ C/1- Operadores Lógicos.cpp
@@ -1,14 +1,71 @@
 #include <stdio.h>
  
-int main() {
+int opOr(int a, int b); 
+int opAnd(int a, int b);
+
  
+int main() {
+ 	int a, b;
+ 	
+ 	printf("Digite dois números A e B: \n");
+	scanf("%d%d",&a,&b);
+ 	 printf("%d", opAnd(a,b));
+	 printf("%d", opOr(a,b));
+ 	
+ 	
+ 	
     return 0;
-    printf("%d", opAnd(1, 2));
+   
 }
 
 
 int opAnd(int a, int b) {
-	int c;
-	c = a & b
-	return c;
+	int r = a & b, i = 0, j, q, vet[32];
+	
+	do {
+		q = r / 2;
+		vet[i] = r % 2;
+		r = q;
+		i++;
+	} while(q != 1 && q > 0);
+		vet[i] = r;
+		printf("AND: ");
+	for(j = i; j > 0; j--) {
+		printf("%d", vet[j]);
+	}	
+	
+	
+	
 }
+
+
+int opOr(int a, int b) {
+	int r = a | b,q, i = 0, vet[32], j;
+	
+		do {
+			q = r / 2;
+			vet[i] = r % 2;
+			r = q;
+			i++;
+			} while (q != 1 && q > 0);
+				vet[i] = r; 
+				printf("\nOR: ");
+			for (j = i; j >= 0; j--) { 
+				printf("%d", vet[j]);
+	}
+	
+	return 0;
+}
+
+
+int OpXor(int a, int b) {
+	int r = a ^ b, q, vet[32], j;
+	
+	do {
+		
+	}
+	
+	
+}
+
+
